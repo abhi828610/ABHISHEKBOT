dfe6843b28bb1b1ae7dca3d0d8b240f98c5b3888
badmash543 <thakurbadmash543@gmail.com>
2025-07-28 02:02:08 +0530

    Update #owner.js

--- Priyansh/commands/#owner.js
+++ Priyansh/commands/#owner.js
@@ -19,11 +19,13 @@ react.includes("OWNER") ||
 react.includes("Sir") ||     
 react.includes("sir")) {
     var msg = {
-        body: "💐🍁★𝙊𝙒𝙉𝙀𝙍 + 𝙈𝘼𝘿𝙀 💦🕊️𝘽𝙔★\n\n✦⸙𝛥︵⸦⸧๏ᆖᆖ๏⸦⸧ 員⸨𝕆༄𒁍≛⃝𝐌𝐑.𝐒𝐀𝐍𝐃𝐄𝐄𝐏 𝐒𝐀𝐕𝐀𝐑𝐈𝐘𝐀🍀🍒💐\n\n★★᭄𝙁𝘼𝘾𝙀𝘽𝙊𝙊𝙆 𝙄𝘿 𝙇𝙄𝙉𝙆 🥀🌿,:\n\n✦ https://www.facebook.com/profile.php?id=61561421229589 ✦ \n🌿🥀𝙁𝙊𝙇𝙇𝙊𝙒 𝙈𝙀 𝙊𝙉\n 𝙄𝙉𝙎𝙏𝘼𝙂𝙍𝘼𝙈🌿🥀😋https://www.instagram.com/sunny_sanwariya?igsh=d3hpMDk1d3BjZGpu ✦ \n🌿🥀𝙁𝙊𝙇𝙇𝙊𝙒 𝙈𝙀 𝙊𝙉\n 𝙏𝙀𝙇𝙀𝙂𝙍𝘼𝙈 𝙐𝙎𝙀𝙍𝙉𝘼𝙈𝙀🌿🥀😋@Yuvi5159`",
-	attachment: fs.createReadStream(__dirname + `/noprefix/Abhi1.jpg`)
+        body: `${name} 🦢🍒•••ꞪɛᏒɛ ɪʂ ɮ❍┼ ❍ωɳɜɽ ɳaʍɜ•••🌷💞
+┏━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┓
+🌸✦✧✧✧✧✰🍒✬✿╭┳✪✪╤───────────➛➣ ★�*★᭄𝗢𝘄𝗻𝗲𝗿 ཫ༄𒁍≛⃝𝐌𝐑.𝐀𝐁𝐇𝐈𝐒𝐇𝐄𝐊 𝐒𝐈𝐍𝐆𝐇🍀🍒💐)✪��★  ✬✿╭┳✪✪╤───────────➛➣🤩🌿✰✧✧✧✧✦🌸 FACEBOOK ID LINK :https://www.facebook.com/profile.php?id=100085068887359
+┗━🕊️━━°❀•°:🎀🧸💙🧸🎀:°•❀°━━💞━┛  ✿`,attachment: fs.createReadStream(__dirname + `/noprefix/Abhi1.jpg`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("📷", event.messageID, (err) => {}, true)
+    api.setMessageReaction("😘", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = async ({ api, event, Currencies, args, utils, client, global }) => {
